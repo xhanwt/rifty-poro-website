eca346c19b06da85a5a6768891a7a223cbf9bbbe
hans334 <84727741+hans334@users.noreply.github.com>
2021-09-27 17:58:47 +0630

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -5,9 +5,7 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener('click', ()=>{
    //Animate Links
     navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+
 
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
