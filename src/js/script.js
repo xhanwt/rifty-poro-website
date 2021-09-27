b2c9b74281da86d3a285a79fa816421fdf9dbd37
hans334 <84727741+hans334@users.noreply.github.com>
2021-09-27 19:11:25 +0630

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -5,8 +5,12 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener('click', ()=>{
    //Animate Links
     navLinks.classList.toggle("open");
-
-
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
+});
+
+$(function () {
+    $('.nav-links li a').bind('click touchstart', function () {
+            $('.hamburger').click();
+    });
 });
