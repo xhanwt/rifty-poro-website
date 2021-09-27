d9c875f8cca104591df99f2c874a2f0df37dc62f
hans334 <84727741+hans334@users.noreply.github.com>
2021-09-27 16:56:47 +0630

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
-$('nav').find('a').click(function(){
-    var $href = $(this).attr('href');
-    var $anchor = $($href).offset();
-    window.scrollTo({left: $anchor.left,top: $anchor.top, behavior: 'smooth'});
-    console.log('runned')
-    return false;
-   
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
 });
